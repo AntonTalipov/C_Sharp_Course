b3c4e05ca8548b3c117f8d855a0a143d2bd39f79
«Anton» <ant.talipov@gmail.com>
2022-11-26 06:37:01 +0300

    first task

--- 1_lesson/1_0/Program.cs
+++ 1_lesson/1_0/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1 task
+Console.WriteLine("Write a number: ");
+
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(num*num);
