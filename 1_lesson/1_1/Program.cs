6c2f55fe29e343ecbf20f94f396dc25cda213229
«Anton» <ant.talipov@gmail.com>
2022-11-26 06:55:03 +0300

    second task

---
+++ 1_lesson/1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2 task
+Console.WriteLine("Write a number1: ");
+
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Write a number2: ");
+
+int num2 = int.Parse(Console.ReadLine());
+int num3=num2*num2;
+if(num3==num1)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+   Console.WriteLine("No"); 
+}
