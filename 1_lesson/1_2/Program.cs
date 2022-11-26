7847cc377c1b84915a44a8832f53d2b0a2789cc1
«Anton» <ant.talipov@gmail.com>
2022-11-26 07:05:43 +0300

    3 task

---
+++ 1_lesson/1_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// 3 task
+Console.WriteLine("Write a number of a day of the week: ");
+int w = int.Parse(Console.ReadLine());
+if (w==1)
+{
+  Console.WriteLine("Monday");  
+}
+else if (w==2)
+{
+ Console.WriteLine("Tuesday");   
+}
+else if (w==3)
+{
+ Console.WriteLine("Wednesday");   
+}
+else if (w==4)
+{
+ Console.WriteLine("Thursday");   
+}
+else if (w==5)
+{
+ Console.WriteLine("Friday");   
+}
+else if (w==6)
+{
+ Console.WriteLine("Saturday");   
+}
+else if (w==7)
+{
+ Console.WriteLine("Monday");   
+}
+else
+{
+ Console.WriteLine("Error");    
+}
