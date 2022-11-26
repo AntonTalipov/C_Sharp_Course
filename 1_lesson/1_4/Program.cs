5cc385263a64af53e3b7ca98f40a9bd7ab84b1ee
«Anton» <ant.talipov@gmail.com>
2022-11-26 08:06:10 +0300

    4 task

---
+++ 1_lesson/1_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу вычисления
+// модуля числа.
+// 6 -> 6
+// -82 -> 82
+
+Console.WriteLine("Write a number:");
+var s = Console.ReadLine();
+int n = s == null ? 0 : int.Parse(s);
+Console.WriteLine($"{Math.Abs(n)}");
