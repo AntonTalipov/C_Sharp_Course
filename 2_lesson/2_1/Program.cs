7d4c2b1012a3fad904e4205504d99ebf3abe22d7
«Anton» <ant.talipov@gmail.com>
2022-11-28 15:48:57 +0300

    task 2_1

---
+++ 2_lesson/2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая выводит случайное
+трехзначное число и на выходе показывает
+последнюю цифру этого числа.
+● 456 -> 6
+● 782 -> 2
+● 918 -> 8 */
+int MaxNumber(int num)
+{
+ Console.WriteLine(num); 
+ return num % 10;
+}
+Console.WriteLine(MaxNumber(new Random().Next(100, 1000)));
