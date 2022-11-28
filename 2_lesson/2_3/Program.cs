6ab20db0327582a8744a9c43dabfcee26408965d
«Anton» <ant.talipov@gmail.com>
2022-11-28 16:48:57 +0300

    task 2_3

---
+++ 2_lesson/2_3/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает на вход
+число и проверяет, кратно ли оно одновременно
+7 и 23.
+● 14 -> нет
+● 46 -> нет
+● 161 -> да */
+
+string Div(int num)
+{
+if (num % 7 == 0 && num % 5 == 0)
+{
+    return "Yes";
+}
+else
+{
+    return "No";
+}
+}
+Console.WriteLine("type a number");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine(Div(num1));
