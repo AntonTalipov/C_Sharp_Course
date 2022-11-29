470fa409586194450cd5fef6355a971669f13428
«Anton» <ant.talipov@gmail.com>
2022-11-29 15:37:58 +0300

    HW task 3

---
+++ 2_lesson/HW_task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 3: Напишите программу, которая выводит третью
+цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.WriteLine("type number");
+int num = int.Parse(Console.ReadLine());
+if (num>99)
+{
+   Console.WriteLine(num.ToString()[2]);
+}
+else
+{
+   Console.WriteLine("There is no third number"); 
+}
