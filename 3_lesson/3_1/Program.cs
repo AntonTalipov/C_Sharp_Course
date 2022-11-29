7263a3b267ccfb0b3ec890ea5c606f5ceaaa0447
«Anton» <ant.talipov@gmail.com>
2022-11-29 16:43:25 +0300

    3_1

---
+++ 3_lesson/3_1/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая по
+заданному номеру четверти, показывает диапазон
+возможных координат точек в этой четверти (x и y) */
+void Dip(int quat)
+{
+    if (quat==1)
+    {
+       Console.WriteLine("Diapazone is x>0 and y>0"); 
+    }
+    else if (quat==2)
+    {
+       Console.WriteLine("Diapazone is x<0 and y>0"); 
+    }
+    else if (quat==3)
+    {
+       Console.WriteLine("Diapazone is x<0 and y<0"); 
+    }
+    else if (quat==4)
+    {
+       Console.WriteLine("Diapazone is x>0 and y<0"); 
+    }
+}
+Dip(3);
