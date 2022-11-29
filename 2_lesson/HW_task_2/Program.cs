73e2f336cfdfcf331a1814e9e42a887ff6ea0413
«Anton» <ant.talipov@gmail.com>
2022-11-29 10:20:47 +0300

    HW task 1

---
+++ 2_lesson/HW_task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая выводит случайное
+трёхзначное число и удаляет вторую цифру этого числа.
+645 -> 65
+782 -> 72
+918 -> 98 */
+
+string DeleteMedium(int num)
+{
+    return num.ToString().Remove(1, 1);
+}
+
+int num1=new Random().Next(100, 1000);
+Console.WriteLine(num1);
+Console.WriteLine(DeleteMedium(num1));
