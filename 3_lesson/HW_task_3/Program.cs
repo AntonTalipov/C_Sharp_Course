ad17b61754c542686b01137efd41740faabb1344
«Anton» <ant.talipov@gmail.com>
2022-11-30 09:19:12 +0300

    HW_task_3

---
+++ 3_lesson/HW_task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает на
+вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27.
+5 -> 1, 8, 27, 64, 125
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+Console.WriteLine("type number, which is more than 0");
+int num1 = int.Parse(Console.ReadLine());
+int num=1;
+if (num1>0)
+{
+   while (num<=num1)
+{
+   Console.WriteLine(Math.Pow(num, 3));
+   num++;
+} 
+}
+else
+{
+    Console.WriteLine("Error! This number is less than 0");
+}
