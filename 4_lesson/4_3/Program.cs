4e3e2da9d3d41523acc4669a10ae886829190f27
«Anton» <ant.talipov@gmail.com>
2022-12-03 15:49:58 +0300

    4_3

---
+++ 4_lesson/4_3/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая выводит
+массив из 8 элементов, заполненный нулями и
+единицами в случайном порядке.
+[1,0,1,1,0,1,0,0] */
+int n = int.Parse(Console.ReadLine());
+int [] arr = new int [n];
+
+void Collection(int [] array)
+{
+    for (int i = 0; i < n; i++)
+    {
+      array[i]= new Random().Next(0, 2);
+      Console.Write(array[i]);
+    }
+}
+Collection(arr);
