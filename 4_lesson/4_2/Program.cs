e77664e09c2b64840d327bc9b702bd7d89f0d9b8
«Anton» <ant.talipov@gmail.com>
2022-12-03 15:06:22 +0300

    4_2

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120 */
+Console.WriteLine("type a number");
+int number =int.Parse(Console.ReadLine());
+int Fractal(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <=num; i++)
+    {
+    sum = sum*i ; 
+    }
+    return sum;
+}
+Console.WriteLine(Fractal(number));
