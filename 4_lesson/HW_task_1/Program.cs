961594e45818a1a060cd2d75f6ec9cedb97ca1d2
«Anton» <ant.talipov@gmail.com>
2022-12-03 16:22:23 +0300

    HW task 1

---
+++ 4_lesson/HW_task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B.*/
+Console.WriteLine("type a number");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("type an extent");
+int num2 = int.Parse(Console.ReadLine());
+int Sum (int a, int b)
+{
+int result = 1;
+for (int i = 0; i < b; i++)
+{
+  result = result * a;
+}
+return result;
+}
+Console.WriteLine(Sum(num1, num2));
