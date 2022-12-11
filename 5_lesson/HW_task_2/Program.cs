641b8b72072d33afb7408e9d759cf29362d3f828
«Anton» <ant.talipov@gmail.com>
2022-12-11 05:47:00 +0300

    hw task 2

---
+++ 5_lesson/HW_task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задайте одномерный массив, заполненный
+случайными числами. Найдите сумму элементов, стоящих
+на нечётных позициях.
+[3, 7, 23, 12] -> 26
+[-4, -6, 4, 67] -> 0 */
+
+Console.WriteLine("type a count of array");
+int n = int.Parse(Console.ReadLine());
+int [] arr = new int [n];
+void Collection(int [] array)
+{ 
+    for (int i = 0; i < n; i++)
+    {
+      array[i]= new Random().Next(1, 100);
+    }
+}
+void Print(int[] arr)
+{
+	for (int i = 0; i < arr.Length; i++)
+	    Console.Write($"{arr[i]}, ");
+	Console.WriteLine();
+}
+void CountArray(int [] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if ( i % 2 == 0)
+        {
+          sum = sum + arr[i];  
+        }
+    }
+    Console.WriteLine( $"sum of elements of odd positions is {sum}");
+}
+Collection(arr);
+Print(arr);
+CountArray(arr);
