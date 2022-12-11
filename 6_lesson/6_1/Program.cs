c0ef6122116a8d4d3b43c13572b2660f8fe7207d
«Anton» <ant.talipov@gmail.com>
2022-12-11 16:33:26 +0300

    6_1

---
+++ 6_lesson/6_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая принимает на вход три
+числа и проверяет, может ли существовать треугольник с сторонами
+такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника
+меньше суммы двух других сторон.*/
+// a<(b+c), b<(a+c), c<(a+b)
+Console.WriteLine("type sides of the triangle");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+void Triangle (int a,int b,int c){
+if (a<(b+c) && b<(a+c) && c<(a+b))
+{
+ Console.WriteLine("it can be a triangle");   
+}
+else
+{
+   Console.WriteLine("it can`t be a triangle");  
+}}
+Triangle(a, b, c);
