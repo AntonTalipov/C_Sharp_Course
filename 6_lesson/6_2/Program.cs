a6968ae2adb0aac76164653d8fc886f684f2c79c
«Anton» <ant.talipov@gmail.com>
2022-12-12 08:23:24 +0300

    6_2

---
+++ 6_lesson/6_2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая будет преобразовывать
+десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10*/
+
+Console.WriteLine("type a number");
+int num = int.Parse(Console.ReadLine());
+string Generation(int num)
+{
+string sum ="";
+while (num >= 1)
+{
+  
+  if (num % 2 > 0)
+  {
+    sum = "1" + sum;
+    num = num / 2; 
+  } 
+  else
+  {
+    sum = "0" + sum;
+    num = (num / 2) - (num % 2);
+  }
+}
+return sum;
+}
+Console.WriteLine($"your number {num} in BIN system is {Generation(num)}");
