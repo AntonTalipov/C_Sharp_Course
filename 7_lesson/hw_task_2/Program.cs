dab3846b45e01efcdf817a2f166a4809f1c742aa
«Anton» <ant.talipov@gmail.com>
2022-12-16 16:19:54 +0300

    hw task 1 and 2

---
+++ 7_lesson/hw_task_2/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Напишите программу, которая на вход
+принимает позиции элемента в двумерном массиве, и
+возвращает значение этого элемента или же указание,
+что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1 7 -> такого числа в массиве нет */
+
+void FillArray(int [,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+       matrix [i, j] = new Random().Next(1, 10);
+    } 
+}
+}
+void PrintArray(int [,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+       Console.Write($"{matrix [i,j]} ");
+    } 
+    Console.WriteLine();
+}
+}
+int [,] matrix = new int [4, 4];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine("type a row of array");
+int row = int.Parse(Console.ReadLine())-1;
+Console.WriteLine("type a column of array");
+int column = int.Parse(Console.ReadLine())-1;
+string CheckNumberInArray (int [,] matrix, int row, int column)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+       if (i == row && j == column)
+       {
+        return $"your number is {matrix[i, j]}";
+       }
+    }    
+}
+return "incorrect row or column";
+}
+Console.WriteLine(CheckNumberInArray (matrix, row, column));
