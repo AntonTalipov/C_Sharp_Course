8790a9f857c5268aa7d3f0b2b59101014eb563d1
«Anton» <ant.talipov@gmail.com>
2022-12-16 09:13:51 +0300

    7_0

---
+++ 7_lesson/7_1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1*/
+
+void FillArray(int [,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+       matrix [i, j] = new Random().Next(-10, 11);
+    } 
+}
+}
+void PrintArray(int [,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+       Console.Write($"{matrix [i,j]} ");
+    } 
+    Console.WriteLine();
+}
+}
+int [,] matrix = new int [3, 4];
+FillArray(matrix);
+PrintArray(matrix);
+
