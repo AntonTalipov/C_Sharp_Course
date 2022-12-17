e3595e5301ec772bb0805c26d26aa28d932d8c94
«Anton» <ant.talipov@gmail.com>
2022-12-17 10:42:56 +0300

    8_1

---
+++ 8_lesson/8_1/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Задайте двумерный массив. Напишите программу,
+которая заменяет строки на столбцы. В случае, если это
+невозможно, программа должна вывести сообщение для
+пользователя.*/
+
+void FillArray(int [,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+       matrix [i, j] = new Random().Next(1, 10);
+    } 
+}
+}
+void PrintArray(int [,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+       Console.Write($"{matrix [i,j]} ");
+    } 
+    Console.WriteLine();
+}
+}
+void ChangeRowAndColumn(int [,] matrix)
+{
+    if ( matrix.GetLength(0) == matrix.GetLength(1))
+    {
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+       Console.Write($"{matrix [j,i]} ");
+    } 
+    Console.WriteLine();
+}
+} 
+else
+{
+    Console.WriteLine("error");
+}
+} 
+int [,] matrix = new int [3, 4];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+ChangeRowAndColumn(matrix);
+
