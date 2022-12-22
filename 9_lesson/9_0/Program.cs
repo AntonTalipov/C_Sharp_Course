4fd109309ae4b3dee5efae4f6c41cfc825e9c2d1
«Anton» <ant.talipov@gmail.com>
2022-12-22 16:49:37 +0300

    9_0

---
+++ 9_lesson/9_0/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет
+все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6" */
+Console.WriteLine("type a number");
+int n = int.Parse(Console.ReadLine());
+void Row (int n)
+{
+    if (n==0)
+    {
+        return;
+    }
+    Row (n-1);
+    Console.Write($" {n} ");
+}
+Row(n);
