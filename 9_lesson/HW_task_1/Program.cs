f6eb064ad3b7de87f25323f34cb716a375416c8a
«Anton» <ant.talipov@gmail.com>
2022-12-24 10:05:30 +0300

    hw task 1

---
+++ 9_lesson/HW_task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задайте значения M и N. Напишите программу, которая выведет все
+чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+M = 1; N = 5 -> "2, 4"
+M = 4; N = 8 -> "4, 6, 8"*/
+Console.WriteLine("type a number a");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("type a number b");
+int n = int.Parse(Console.ReadLine());
+void Row(int m, int n)
+	{
+	    
+	    if (m > n) return;
+        if (m % 2 == 0)
+        {
+        Console.Write($" {m} ");
+        }
+	    Row(m+1, n);
+	}
+	
+	Row(m,n);
