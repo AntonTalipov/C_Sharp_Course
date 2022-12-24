8ec726410b28a4f835dfee881308ae7fa7e8ffc3
«Anton» <ant.talipov@gmail.com>
2022-12-24 08:32:55 +0300

    9_1

--- 9_lesson/9_1/Program.cs
+++ 9_lesson/9_1/Program.cs
@@ -2,3 +2,34 @@
 рекурсивно выведет все натуральные числа в промежутке от M до N.
 M = 1; N = 5 -> "1, 2, 3, 4, 5"
 M = 4; N = 8 -> "4, 6, 7, 8*/
+/*Console.WriteLine("type a number1");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("type a number2");
+int n = int.Parse(Console.ReadLine());
+void Row (int m, int n)
+{ 
+    if (m < n)
+    {
+        if (n-1 == m)
+        {
+            return;
+        }
+    Row (n-1, m);
+    Console.Write($" {n} ");
+    }
+    else 
+    {
+        Row (m-1, n);
+    Console.Write($" {m} ");
+}}
+Row(m,n);*/
+void Numbers(int m, int n)
+	{
+	    
+	    if (m > n) return;
+	    
+	    Console.Write($" {m} ");
+	    Numbers(m+1, n);
+	}
+	
+	Numbers(5,8);
